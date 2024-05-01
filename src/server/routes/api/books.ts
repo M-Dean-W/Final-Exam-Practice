6a94e768b83361ef71fbea033d0ae2bba6ab44c7
MD <mdeanwill@gmail.com>
2024-05-01 16:28:33 -0700

    started routes

---
+++ src/server/routes/api/books.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express'
+// import db from '../../db';
+// import * as jwt from 'jsonwebtoken'
+// import config from '../../config';
+
+const router = Router();
+
+export default router;
---
+++ src/server/routes/auth/login.ts
@@ -0,0 +1,29 @@
+import { Router } from "express";
+// import db from "../../db";
+import { compareHash } from "../../services/passwords";
+import * as jwt from 'jsonwebtoken'
+import config from "../../config";
+
+const router = Router();
+
+// router.post('/', async (req, res) => {
+//     const email = req.body.email
+//     const password = req.body.password
+//     try {
+//         const [userFound] = await db.users.findUser('email', email)
+//         if (userFound && compareHash(password, userFound.password)) {
+//            const token = jwt.sign(
+//             { userid: userFound.id, email: userFound.email },
+//             config.jwt.secret,
+//             { expiresIn:'15d' }
+//         ) 
+//            return res.json(token)
+//         }
+//         return res.status(401).json({ message: 'invalid credentials' })
+//     } catch (error) {
+//         console.log(error)
+//         res.status(500).json({ message: 'Internal Server Error', error })
+//     }
+// });
+
+export default router
