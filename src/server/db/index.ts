e6e721460aa23ef2a0c6ec2900b918c42bd8ed26
MD <mdeanwill@gmail.com>
2024-05-16 15:37:46 -0700

    started routes

---
+++ src/server/db/index.ts
@@ -0,0 +1,9 @@
+import * as books from './tables/books'
+import * as users from './tables/users'
+import * as cat from './tables/categories'
+
+export default{
+    books,
+    users,
+    cat
+}
---
+++ src/server/routes/auth/register.ts
@@ -0,0 +1,7 @@
+import Router from "express";
+import db from '../../db'
+import tokenCheck from "../../middlewares/tokenCheck";
+
+const router = Router()
+
+export default router
