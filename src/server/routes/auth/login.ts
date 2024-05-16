e39354dbf5885ea84c779b3dfac43401e807c998
MD <mdeanwill@gmail.com>
2024-05-16 16:05:09 -0700

    added login and reg routes

--- src/server/routes/auth/login.ts
+++ src/server/routes/auth/login.ts
@@ -1,7 +1,30 @@
 import Router from "express";
 import db from '../../db'
-import tokenCheck from "../../middlewares/tokenCheck";
+import { compareHash } from "../../services/password";
+import * as jwt from 'jsonwebtoken'
+import config from "../../config";
 
 const router = Router()
+
+router.post('/', async (req,res) => {
+    const email = req.body.email
+    const password =req.body.password
+    try {
+        const [userFound] = await db.users.findUser('email', email)
+        if (userFound && compareHash(password, userFound.password)) {
+            const token = jwt.sign(
+                { userid: userFound.id, email: userFound.email },
+                config.jwt.secret,
+                { expiresIn:'15d' }
+            ) 
+               return res.json(token)
+            }
+            return res.status(401).json({ message: 'invalid credentials' })
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: 'Server Error', error})
+    }
+})
 
 export default router
--- src/server/routes/auth/register.ts
+++ src/server/routes/auth/register.ts
@@ -1,7 +1,26 @@
 import Router from "express";
 import db from '../../db'
-import tokenCheck from "../../middlewares/tokenCheck";
+import { compareHash, generateHash } from "../../services/password";
+import * as jwt from 'jsonwebtoken'
+import config from "../../config";
 
 const router = Router()
+
+router.post('/', async (req,res) => {
+    const newUser =req.body
+    try {
+        newUser.password = generateHash(newUser.password)
+        const result = await db.users.registerUser(newUser)
+        const token = jwt.sign(
+            { userid: result.insertId, email: newUser.email, created_at: newUser.created_at },
+            config.jwt.secret,
+            { expiresIn:'15d' }
+        )
+        res.json(token)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: 'Server Error', error})
+    }
+})
 
 export default router
