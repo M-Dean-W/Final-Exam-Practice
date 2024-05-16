c9e1e5de6336c362bd3bbf2351273b1a17ece58a
MD <mdeanwill@gmail.com>
2024-05-16 15:53:12 -0700

    category route done

--- src/server/routes/api/categories.ts
+++ src/server/routes/api/categories.ts
@@ -4,4 +4,34 @@ import tokenCheck from "../../middlewares/tokenCheck";
 
 const router = Router()
 
+router.get('/:id', async (req, res) => {
+
+    try {
+        const id = parseInt(req.params.id, 10)
+        const categories = await db.cat.getOneCategory(id)
+        res.json(categories)
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: 'Internal Server Error', error})
+    }
+    
+})
+
+router.get('/', async (req, res) => {
+
+    try {
+        
+        const categories = await db.cat.getAllCategories()
+        res.json(categories)
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: 'Internal Server Error', error})
+    }
+    
+    
+})
+
+
 export default router
