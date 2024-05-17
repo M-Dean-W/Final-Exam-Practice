4a931233733b4f4585d320bf7bb6fee3e0e1c2ea
MD <mdeanwill@gmail.com>
2024-05-16 17:41:00 -0700

    added add books view

--- src/client/views/AddBooks.tsx
+++ src/client/views/AddBooks.tsx
@@ -2,27 +2,76 @@ import React, { useState, useEffect } from "react";
 import Card from 'react-bootstrap/Card'
 import Container from 'react-bootstrap/Container'
 import { fetcher } from "../services/fetcher";
+import Form from 'react-bootstrap/Form'
+import { Category } from "../types";
+import { useNavigate } from "react-router-dom";
+import { Book } from "../types";
 
-interface AddBooksProps {}
+interface AddBooksProps { }
 
 const AddBooks = (props: AddBooksProps) => {
-    const [data, setData] = useState("");
+    const [author, setAuthor] = useState('')
+    const [title, setTitle] = useState('')
+    const [price, setPrice] = useState<number>(0)
+    const [catID, setCatID] = useState<number | null>(null)
+    const [categories, setCategories] = useState<Category[]>([]);
+    const navigate = useNavigate()
+    const token = localStorage.getItem('token')
+
+
 
     useEffect(() => {
-        
-          fetcher("/api/hello", "GET").then((data) => setData(data.message));
-       
+
+        fetcher("/api/categories", "GET").then((categories) => setCategories(categories));
+
     }, []);
+
+    const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        if (!catID) {
+            alert('Choose Category')
+            return
+        }
+
+        const bookData: Omit<Book, 'id' | 'created_at'> = {
+            category_id:catID,
+            author:author,
+            title:title,
+            price:price
+        }
+
+        fetcher('/api/books', 'POST', bookData)
+        .then(data => navigate(`/books/${data.id}`))
+    
+    
+    }
 
     return (
         <Container>
             <Card>
                 <Card.Title></Card.Title>
-                <Card.Subtitle></Card.Subtitle>
                 <Card.Body>
-                <Card.Text>
-                    Welcome to the AddBookspage for the Bookstore. Login or Register to gain access to editing and adding books!
-                </Card.Text>
+                    <Form>
+                        <Form.Select value={catID ?? ''} onChange={(e)=>setCatID(Number(e.target.value))}>
+                            <option>Select Category</option>
+                            {categories.map(category =>(
+                                <option key={category.id} value={category.id}>{category.name}</option>
+                            ))}
+                        </Form.Select>
+                        <Form.Group>
+                            <Form.Label>Book Title:</Form.Label>
+                            <Form.Control type="text" value={title } onChange={(e)=> setTitle(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Book Author:</Form.Label>
+                            <Form.Control type="text" value={author } onChange={(e)=>setAuthor(e.target.value)} />
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Book Price:</Form.Label>
+                            <Form.Control type="text" value={price } onChange={(e)=>setPrice(Number(e.target.value))} />
+                        </Form.Group>
+                    </Form>
                 </Card.Body>
                 <Card.Footer></Card.Footer>
             </Card>
